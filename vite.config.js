bce4c64ed984abbfb3aea784619fa7807b2c077e
hrithikpandey786 <hrithikpandey786@gmail.com>
2024-08-24 03:40:53 +0530

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Configure the dev server to handle routing
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      // Ensure that all routes fallback to index.html in production
+      output: {
+        entryFileNames: 'index.html',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
